af02c1d8a805073412bf254ff0fc47f8148e20d8
mashafrancis <francismasha96@gmail.com>
2018-09-24 06:01:37 +0300

    Add test for get category

--- app/tests/v1/test_category.py
+++ app/tests/v1/test_category.py
@@ -20,6 +20,36 @@ class CategoryTests(BaseTests):
         self.assertTrue(data['message'] == u"A new category has been added successfully.")
         self.assertFalse(data['message'] == u"Category has been added successfully.")
 
+    def test_get_all_categories(self):
+        """Tests API can get all orders (GET)"""
+        access_token = self.user_token_get()
+
+        # Test for no category found.
+        response = self.client().get('/api/v1/category',
+                                     headers=dict(Authorization="Bearer " + access_token))
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'Not Found')
+        self.assertEqual(data['message'], u"Sorry, No Category found! Create one.")
+        self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 401)
+
+        # Test user cannot delete non existent categories
+        response = self.client().delete('/api/v1/category',
+                                        headers=dict(Authorization="Bearer " + access_token))
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'Not Found')
+        self.assertEqual(data['message'], u"There is no category here!")
+        self.assertEqual(response.status_code, 404)
+
+        # Test for categories found.
+        response = self.client().post('/api/v1/category', data=self.order,
+                                      content_type='application/json',
+                                      headers=dict(Authorization="Bearer " + access_token))
+        self.assertEqual(response.status_code, 201)
+        response = self.client().get('/api/v1/category',
+                                     headers=dict(Authorization="Bearer " + access_token))
+        self.assertEqual(response.status_code, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
